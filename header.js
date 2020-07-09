b71cb0a0fcb95f9b10606513248faeb08997da86
Piyush Porwal <pp14ush@github.com>
2020-07-09 17:35:47 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 1000){
-			$("nav").css({"background-color" : "rgba(0,0,0,0.9)"})
-		}
-		else {
-			$("nav").css({"background-color" : "rgba(0,0,0,0)"})
-		}
-	})
+	
 	var s = skrollr.init();
 });
 
